2353e2b616ed386bd032249ad486ea6e5b88c4d7
kangju2000 <kangju2000@naver.com>
2022-03-16 15:45:24 +0900

    환경변수 설정

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,11 @@
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+app.use(cookieParser()); //토큰을 쿠키에 저장하기 위해
 
 const userSchema = mongoose.Schema({
     name: {
@@ -19,7 +26,7 @@ const userSchema = mongoose.Schema({
         maxlength: 50,
     },
     role: {
-        type: Number,
+        type: Number,  // 1이면 관리자, 0은 일반
         default: 0,
     },
     image: String,
@@ -33,3 +40,60 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
+
+userSchema.pre("save", function (next) {
+    var user = this;
+    if (user.isModified("password")) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        // 그 외에는 그냥 내보낸다
+        next();
+    }
+});
+
+// 로그인 - 비밀번호 비교
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // 입력된 비밀번호와 데이터베이스에 있는 암호화된 비밀번호가 같은지 확인(비교) -> 평문을 암호화해서 비교
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch); // 즉, true
+    });
+};
+
+// 로그인 - 토큰 생성
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+    // jsonwebtoken을 이용해서 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), "secretToken");
+    // user._id + 'secretToken' = token 을 통해 토큰 생성
+    // 토큰 해석을 위해 'secretToken' 입력 -> user._id 가 나옴
+    // 토큰을 가지고 누구인지 알 수 있는 것
+    user.token = token;
+
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
+
+// auth 인증 - 복호화 (토큰을 디코드)
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+
+    jwt.verify(token, "secretToken", function (err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({ _id: decoded, token: token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user);
+        });
+    });
+};
+
--- server/server.js
+++ server/server.js
@@ -3,14 +3,17 @@ const cors = require("cors");
 const app = express();
 const test = require("./routes/test");
 const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
-require("dotenv").config({ path: "../.env" });
+
+const port = 5000;
+const config = require("../config/key");
 
 const mongoose = require("mongoose");
 mongoose
-    .connect(process.env.DB_URI, {
+    .connect(config.mongoURI, {
         dbName: "board",
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -18,13 +21,15 @@ mongoose
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.log(err));
 
-// let db = mongoose.connection;
-
 app.use(cors());
 app.use("/api", test);
 app.get("/api/hello", (req, res) => {
     res.send("하이요");
 });
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
 app.post("/register", (req, res) => {
     const user = new User(req.body);
     user.save((err, userInfo) => {
@@ -35,6 +40,47 @@ app.post("/register", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
+app.post("/api/users/login", (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "이메일에 해당하는 유저가 없습니다.",
+            });
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 틀렸습니다.",
+                });
+
+            // 비밀번호까지 맞다면 토큰을 생성
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // 정상적일 경우 토큰을 쿠키나 로컬스토리지 등에 저장
+                // 쿠키에 저장
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id });
+            });
+        });
+    });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+    // 여기까지 미들웨어(auth.js)를 통과해 왔다는 얘기는 Authentication이 True라는 말
+    // 클라이언트에게 유저 정보 전달
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true, // role이 0이면 일반 유저, 그외는 관리자
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image,
+    });
 });
