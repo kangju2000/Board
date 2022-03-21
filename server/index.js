8fd6d0715d715be5870760712fdad381ee872bfb
kangju2000 <kangju2000@naver.com>
2022-03-22 00:54:49 +0900

    aaaa

--- server/index.js
+++ server/index.js
@@ -27,10 +27,10 @@ mongoose
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "board-app/build")));
+app.use(express.static(path.join(__dirname, "../board-app/build")));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/board-app/build/index.html"));
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../board-app/build/index.html"));
 });
 
 app.use("/api", test);
